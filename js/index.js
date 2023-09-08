96ddc71f700af9948fb936c9cc767d3876308d51
yuanq20 <yuanq_20@163.com>
2023-09-08 19:03:34 +0800

    index

--- js/index.js
+++ js/index.js
@@ -6,7 +6,11 @@ $(function(){
     init_ctn();
 
     $("#a_home").click(function(){
-        alert("欢迎访问 Github ！")
+        alert("欢迎光临，请上座")
+    });
+
+    $("#a_github").click(function(){
+        alert("马上跳转到我的小站 Github，欢迎吐槽 ！")
         window.open("https://github.com/yuanq20");
     });
 
